efb95b6affa588019bc7074f300a6321f6cb83a1
damianzydowicz <damian.zydowicz@gmail.com>
2025-02-11 18:59:59 +0100

    Added logs in GameManager

--- src/main/java/com/dzydowicz/scratchgame/game/GameManager.java
+++ src/main/java/com/dzydowicz/scratchgame/game/GameManager.java
@@ -6,11 +6,13 @@ import com.dzydowicz.scratchgame.dto.Result;
 import com.dzydowicz.scratchgame.matrix.IMatrixGenerator;
 import com.dzydowicz.scratchgame.reward.IRewardCalculator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 class GameManager implements IGameManager {
     private final ConfigDTO config;
     private final int betAmount;
@@ -18,14 +20,18 @@ class GameManager implements IGameManager {
     private final IRewardCalculator rewardCalculator;
 
     public Result playGame() {
+        log.info("Starting game with bet amount {}", betAmount);
+
         int rows = config.getRows() != null ? config.getRows() : 3;
         int columns = config.getColumns() != null ? config.getColumns() : 3;
 
         Cell[][] matrix = matrixGenerator.generateMatrix(config, rows, columns);
+        log.info("Matrix generated, proceeding to calculate rewards");
 
         Result result = rewardCalculator.calculateReward(config, matrix, betAmount, rows, columns);
         result.setMatrix(convertMatrixToList(matrix));
 
+        log.info("Calculated rewards, finishing game");
         return result;
     }
 
